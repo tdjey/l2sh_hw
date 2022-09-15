d651f68509d4312ba555d706e4c8458c4651b1ad
tdjey <shfartem@mail.ru>
2022-09-15 12:16:06 +0300

    variations added

--- main.cpp
+++ main.cpp
@@ -15,14 +15,26 @@ void print_results() {
 }
 
 int main() {
-    string name, surname, book;
-    cout << "Enter your name and surname, please\n";
-    cin >> name >> surname;
-    cout << "Enter your favourite book name, please\n";
-    getline(cin, book);
-    getline(cin, book);
-    ofstream output;
-    output.open("answers.txt", ofstream::app);
-    output << name + " " + surname + ": " + book + ".\n";
-    output.close();
+    string type;
+    cout << "Enter 1 to start the quiz, 2 to get the results\n";
+    cin >> type;
+    while (type != "1" && type != "2") {
+        cout << "Enter 1 to start the quiz, 2 to get the results\n";
+        cin >> type;
+    }
+    if (type == "1") {
+        string name, surname, book;
+        cout << "Enter your name and surname, please\n";
+        cin >> name >> surname;
+        cout << "Enter your favourite book name, please\n";
+        getline(cin, book);
+        getline(cin, book);
+        ofstream output;
+        output.open("answers.txt", ofstream::app);
+        output << name + " " + surname + ": " + book + ".\n";
+        output.close();
+    }
+    else {
+        print_results();
+    }
 }
