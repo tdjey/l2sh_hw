579a218ee8ccea65d8559659523196518d7e9cf7
tdjey <shfartem@mail.ru>
2022-09-15 12:07:58 +0300

    questions file added and main.cpp updated

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    string name, surname, book;
+    ifstream input;
+    string question, answer;
+    input.open("questions.txt");
+    ofstream output;
+    string name, surname;
     cout << "Enter your name and surname, please\n";
     cin >> name >> surname;
-    cout << "Enter your favourite book name, please\n";
-    getline(cin, book);
-    getline(cin, book);
-    freopen("ans.txt", "a", stdout);
-    cout << name + " " + surname + ": " + book + ".\n";
+    output.open("answer.txt", fstream::app);
+    output << name << " " << surname;
+    getline(cin, answer);
+    while (!input.eof()) {
+        getline(input, question);
+        if (question == "")
+            cout << "No questions 4 u)\n";
+        else {
+            cout << question << "\n";
+            getline(cin, answer);
+            output << ", " << answer;
+        }
+    }
+    output << "\n";
+    input.close();
+    output.close();
 }
