760043a911e7cc841cdb1a10a125c53fee91f6e8
Timmm005 <66874161+Timmm005@users.noreply.github.com>
2022-09-22 12:47:46 +0300

    added the ability to modify questions

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ void print_results(int width) {
         getline(input, user_info, '~');
         if (shift_counter % questions.size() == 0)
             cout << "\n";
-        if ((shift_counter + 1) % question_counter == 0) 
+        if ((shift_counter + 1) % question_counter == 0)
             answers_map[user_info] += 1;
         if (user_info == "\n" || user_info == "")
             continue;
@@ -66,7 +66,7 @@ void print_results(int width) {
     cout << "\n";
     int max_ans = 0;
     string ans;
-    for (auto cur : answers_map) 
+    for (auto cur : answers_map)
         if (cur.second > max_ans) {
             ans = cur.first;
             max_ans = cur.second;
@@ -79,10 +79,10 @@ void print_results(int width) {
 
 int main() {
     string type;
-    cout << "Enter 1 to start the quiz, 2 to get the results\n";
+    cout << "Enter 1 to start the quiz, 2 to get the results, 3 to modify the questions\n";
     cin >> type;
-    while (type != "1" && type != "2") {
-        cout << "Enter 1 to start the quiz, 2 to get the results\n";
+    while (type != "1" && type != "2" && type != "3") {
+        cout << "Enter 1 to start the quiz, 2 to get the results, 3 to modify the questions\n";
         cin >> type;
     }
     if (type == "1") {
@@ -109,10 +109,61 @@ int main() {
         input.close();
         output.close();
     }
-    else {
+
+    else if (type == "2") {
         cout << "Enter the width of the table ans, please: \n";
         int width;
         cin >> width;
         print_results(width);
+    }
+
+    else
+    {
+        fstream q;
+        string _;
+        int mode = 0;
+
+        cout << "1 to delete a question, 2 to add\n";
+        while (mode != 1 && mode != 2) { cin >> mode; }
+
+        if (mode == 1) //delete
+        {
+            fstream newQustions;
+            q.open("questions.txt");
+            newQustions.open("newQustions.txt", ios_base::app);
+
+            string questionLine, conf;
+            getline(cin, _);
+            while (getline(q, questionLine))
+            {
+                cout << questionLine << "\n";
+                cout << "Would you like to delete this question?('n' to pass, 'y' to delete)\n";
+                while (conf != "n" && conf != "y") { cin >> conf; }
+                if (conf == "n") { newQustions << questionLine << "\n"; }
+                conf = "";
+            }
+            newQustions.close();
+            q.close();
+            remove("questions.txt");
+            rename("newQustions.txt", "questions.txt");
+        }
+
+        else //add
+        {
+            q.open("questions.txt", ios_base::app);
+            string newQuestion;
+            getline(cin, _);
+            while (newQuestion != "/stop")
+            {
+                cout << "Input a new question or '/stop' to stop adding questions\n";
+                getline(cin, newQuestion);
+                if (newQuestion != "/stop") { q << newQuestion << "\n"; }
+            }
+            q.close();
+        }
+
+        remove("answers.txt");
+        cout << "old answers.txt was deleted\n";
+
     }
 }
